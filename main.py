9fe55fcbad8b4b076e76c22fb392f466e19508b6
mattpower <matteo.rocco.lt@gmail.com>
2025-05-14 13:46:51 +0200

    Save/Load models implemented

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import torch.nn as nn
+import torch
+import os
 from CNNTrainingTest import testCNN, trainingCNN
 from MyLeNetCNN import MyLeNetCNN
 from PrepareData import prepare_data
@@ -7,18 +9,26 @@ from PerformanceEvaluation import *
 from StreamEvaluation import streamEvaluation
 from CustomTransform import buildAlexNetTransformations, buildLeNetTransformations
 
-
-# Set number of experiments
-num_exp = 50
-image_path = 'path/to/dataset'
-csv_path = 'GenderRecognitionViaHands/11kHands_gender_afifi2017_/Progetto/HandInfo.csv'
-num_train = 400
-num_test = 200
-
 # Create the networks
 leNet = MyLeNetCNN(num_classes=2)
 alexNet1 = torchvision.models.alexnet(weights=torchvision.models.AlexNet_Weights.IMAGENET1K_V1)
 alexNet2 = torchvision.models.alexnet(weights=torchvision.models.AlexNet_Weights.IMAGENET1K_V1)
+
+# Set number of experiments
+num_exp = 5
+image_path = '/home/mattpower/Downloads/Hands'
+net_palmar_model_path = '/home/mattpower/Documents/backup/Magistrale/Sapienza/ComputerScience/FDS/SecretProject/GenderRecognitionViaHands/11kHands_gender_afifi2017_/Progetto/models/net_palmar.pth'
+net_dorsal_model_path = '/home/mattpower/Documents/backup/Magistrale/Sapienza/ComputerScience/FDS/SecretProject/GenderRecognitionViaHands/11kHands_gender_afifi2017_/Progetto/models/net_dorsal.pth'
+csv_path = '/home/mattpower/Documents/backup/Magistrale/Sapienza/ComputerScience/FDS/SecretProject/GenderRecognitionViaHands/11kHands_gender_afifi2017_/Progetto/HandInfo.csv'
+num_train = 50
+num_test = 20
+
+# Set the networks
+net_palmar = leNet
+net_dorsal = alexNet2
+
+weight_palmar = 0.4
+weight_dorsal = 0.6
 
 # Customize AlexNet1
 # Update the final layer to output 2 classes
@@ -41,13 +51,6 @@ for param in alexNet2.parameters():
     param.requires_grad = False
 for param in alexNet2.classifier[6].parameters():
     param.requires_grad = True
-
-# Set the networks
-net_palmar = leNet
-net_dorsal = alexNet2
-
-weight_palmar = 0.4
-weight_dorsal = 0.6
 
 # Build the tranformations for the networks
 palmar_transforms = buildAlexNetTransformations()
@@ -73,13 +76,22 @@ weights_palmar_dorsal = [weight_palmar, weight_dorsal]
 # Prepare data
 data_struct = prepare_data(csv_path=csv_path, num_exp=num_exp, num_train=num_train, num_test=num_test)
 
-# Training the networks
-print('Begin Palm Training\n')
-train_loss_p = trainingCNN(net=net_palmar, transforms=transforms, data_struct=data_struct, image_path=image_path, palmar_dorsal='palmar', tot_exp=num_exp)
-print('\nFinished Palm Training\n')
-print('Begin Dorsal Training\n')
-train_loss_d = trainingCNN(net=net_dorsal, transforms=transforms, data_struct=data_struct, image_path=image_path, palmar_dorsal='dorsal', tot_exp=num_exp)
-print('\nFinished Dorsal Training\n')
+# Check if models are available
+if os.path.exists(net_palmar_model_path) and os.path.exists(net_dorsal_model_path):
+    leNet.load_state_dict(torch.load(net_palmar_model_path, weights_only=True))
+    print(f"Loaded model from {net_palmar_model_path}")
+    alexNet2.load_state_dict(torch.load(net_dorsal_model_path, weights_only=True))
+    print(f"Loaded model from {net_dorsal_model_path}")
+else:
+    print(f"Model not found at {net_palmar_model_path} or {net_dorsal_model_path}. Training from scratch.\n")
+
+    # Training the networks
+    print('Begin Palm Training\n')
+    train_loss_p = trainingCNN(net=net_palmar, transforms=transforms, data_struct=data_struct, image_path=image_path, palmar_dorsal='palmar', tot_exp=num_exp)
+    print('\nFinished Palm Training\n')
+    print('Begin Dorsal Training\n')
+    train_loss_d = trainingCNN(net=net_dorsal, transforms=transforms, data_struct=data_struct, image_path=image_path, palmar_dorsal='dorsal', tot_exp=num_exp)
+    print('\nFinished Dorsal Training\n')
 
 # Test the networks
 print('Begin Palm Testing')
@@ -101,8 +113,11 @@ calculate_confusion_matrix(dorsal_labels, dorsal_predicted)
 calculate_confusion_matrix(un_labels, un_predicted)
 
 # Calculate the loss plot
-calculate_loss_plot(train_loss_p)
-calculate_loss_plot(train_loss_d)
+if not os.path.exists(net_palmar_model_path):
+    calculate_loss_plot(train_loss_p)
+
+if not os.path.exists(net_dorsal_model_path):
+    calculate_loss_plot(train_loss_d)
 
 # Print the performance metrics
 print("\nPerformance Metrics\n")
@@ -124,3 +139,6 @@ print("\nAccuracy Unified Network: ", calculate_accuracy(un_labels, un_predicted
 print("Precision Unified Network: ", calculate_precision(un_labels, un_predicted))
 print("Recall Unified Network: ", calculate_recall(un_labels, un_predicted))
 print("F1 Score Unified Network: ", calculate_f1_score(un_labels, un_predicted),"\n")
+
+torch.save(net_palmar.state_dict(), net_palmar_model_path)
+torch.save(net_dorsal.state_dict(), net_dorsal_model_path)
